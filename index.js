4d312b28de64d1f4570b4ce9988450870ed37ea0
George Martinez <gemartinez@fi.uba.ar>
2022-10-05 02:19:35 -0300

    exercises phonebook 3.7 to 3.8 done

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
+    ].join(' ')
+}))
 
 let data = [
     { 
@@ -84,8 +95,6 @@ app.post('/api/persons', (request, response) => {
     }
 
     data.push(person)
-
-    console.log(data);
 
     return response.status(200).json(person)
 })
